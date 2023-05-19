f3ec575f2156e407cc7b4e59498be654f280bdbe
Hyeondoonge <migu554@naver.com>
2023-05-19 20:58:39 +0900

    update babel-loder, preset-react 의존성 추가 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'Analogue Clock'
