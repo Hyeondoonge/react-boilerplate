828fd49842ab95fad388cc2108ac2395666ce4a0
Hyeondoonge <migu554@naver.com>
2023-05-19 20:57:10 +0900

    update html webpack plugin 추가 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,11 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Analogue Clock'
+    })
+  ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
